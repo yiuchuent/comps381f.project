f06071fa8022b2aaf644fe1215ced50567feae52
wai <vico19952727@hotmail.com>
2016-11-21 07:52:17 -0800

    help me to debug

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
-var session = require('express-session');
+var session = require('cookie-session');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var mongourl = 'mongodb://localhost:27017/test';
-
+var qs = require('querystring');
 var SECRETKEY = 'This is secretkey for comps381f project';
+var bodyParser = require('body-parser');
+
+
+var data = '';
 
 app.set('view engine', 'ejs');
 
@@ -15,12 +19,44 @@ app.use(session({
 }));
 
 app.use(express.static(__dirname +  '/public'));
+app.use(bodyParser.json());
 
-app.get("/login", function(req,res) {
-	res.sendFile(__dirname + '/public/login.html');
-	//res.render('index.ejs');
-	
+app.post("/register", function(req,res) {
+
+	console.log(req.body.userId);
+      MongoClient.connect(mongourl,function(err,db) {
+      console.log('Connected to db');
+
+      create(db,req.body.userId,req.body.password,req.body.email,
+	function(result) {
+          db.close();
+          if (result.insertedId != null) {
+            res.status(200);
+            res.end('Inserted: ' + result.insertedId);
+          } else {
+            res.status(500);
+            res.end(JSON.stringify(result));
+          }
+      });
+    });
 });
+
+function create(db,userId,password,email,callback) {
+  db.collection('test').insertOne({
+    "userId" : userId,
+    "password" : password,
+    "email" : email,
+  }, function(err,result) {
+    //assert.equal(err,null);
+    if (err) {
+      result = err;
+      console.log("insertOne error: " + JSON.stringify(err));
+    } else {
+      console.log("Inserted _id = " + result.insertedId);
+    }
+    callback(result);
+  });
+}
 
 app.get("/logout", function(req,res) {
 	req.session = null;
